8251134054724e4d2f9630ad6f9cacc6902ac0da
EC2 Default User <ec2-user@ip-172-31-40-52.ap-south-1.compute.internal>
2026-05-13 06:23:05 +0000

    Canada level DevOps project

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "canada-devops-vpc"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "canada-devops-igw"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "cluster_name" {
+  default = "canada-devops-cluster"
+}
+
+variable "aws_region" {
+  default = "ap-south-1"
+}
